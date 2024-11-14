baad5c71959516d462d80a80b69eaae2abcfdb14
batalova-kira <batalovakira@gmail.com>
2024-11-14 18:48:32 +0200

    prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,3 @@
-import styles from "./page.module.css";
-import course from "./course.json";
-import { Lesson } from "./lesson";
-
-async function getAllCourses(): Promise<typeof course> {
-    return new Promise((resolve) => {
-        setTimeout(() => resolve(course), 1000);
-    });
-}
-
 export default async function Home() {
-    const courses = await getAllCourses();
-    return (
-        <main className={styles.main}>
-            <ul>
-                {courses.lessons.map((lesson) => {
-                    return (
-                        <li key={lesson.name}>
-                            <Lesson title={lesson.title} name={lesson.name} />
-                        </li>
-                    );
-                })}
-            </ul>
-        </main>
-    );
+    return <main></main>;
 }
